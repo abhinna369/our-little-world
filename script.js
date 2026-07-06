8b4d470e0cedf5d5350db7755c6ce045de53ca3b
abhinna369 <sundruuu25@gmail.com>
2026-07-06 20:02:44 +0530

    Beach interaction

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text = [
+const messages = [
   "Hi Chuiimuii... ❤️",
   "Today I want to take uuu on a journey...",
   "Not through a website...",
@@ -7,36 +7,74 @@ const text = [
 ];
 
 const typing = document.getElementById("typing");
-const button = document.getElementById("startBtn");
-
-button.style.display = "none";
+const startBtn = document.getElementById("startBtn");
 
 let line = 0;
 let char = 0;
 
+startBtn.style.display = "none";
+
 function typeWriter() {
 
-  if (line >= text.length) {
-    button.style.display = "inline-block";
-    return;
-  }
+    if (line >= messages.length) {
+        startBtn.style.display = "inline-block";
+        return;
+    }
 
-  if (char < text[line].length) {
-    typing.innerHTML += text[line].charAt(char);
-    char++;
-    setTimeout(typeWriter, 50);
-  } else {
-    typing.innerHTML += "<br><br>";
-    line++;
-    char = 0;
-    setTimeout(typeWriter, 700);
-  }
+    if (char < messages[line].length) {
+        typing.innerHTML += messages[line].charAt(char);
+        char++;
+        setTimeout(typeWriter, 45);
+    } else {
+        typing.innerHTML += "<br><br>";
+        line++;
+        char = 0;
+        setTimeout(typeWriter, 700);
+    }
 }
 
 window.onload = () => {
-  setTimeout(typeWriter, 800);
+    setTimeout(typeWriter, 800);
 };
 
-button.addEventListener("click", () => {
-  alert("🌊 Welcome! In the next part, our Beach Journey begins...");
+const intro = document.getElementById("intro");
+const beach = document.getElementById("beach");
+
+startBtn.addEventListener("click", () => {
+
+    intro.style.opacity = "0";
+
+    setTimeout(() => {
+
+        intro.classList.add("hidden");
+
+        beach.classList.remove("hidden");
+
+        beach.style.opacity = "0";
+
+        setTimeout(() => {
+            beach.style.opacity = "1";
+        }, 100);
+
+    }, 1200);
+
+});
+
+const shell = document.querySelector(".shell");
+const memory = document.getElementById("memory");
+const continueBtn = document.getElementById("continueBtn");
+
+shell.addEventListener("click", () => {
+
+    memory.classList.remove("hidden");
+
+    continueBtn.style.display = "inline-block";
+
+    shell.style.transform = "scale(1.2)";
+});
+
+continueBtn.addEventListener("click", () => {
+
+    alert("🌧️ Rain Chapter is coming in the next update ❤️");
+
 });
